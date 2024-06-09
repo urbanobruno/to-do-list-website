285847d2b8b047d488acbf35cd12b7254edfc8bb
Bruno Urbano <bruno.urbano.rocha@gmail.com>
2024-06-08 22:40:16 -0300

    ajustes forms

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export const TodoList = () => {
+  const [tarefas, setTarefas] = useState([]);
+
+  useEffect(() => {
+    fetchTarefas();
+  }, []);
+
+  const fetchTarefas = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/task");
+      if (response.ok) {
+        const data = await response.json();
+        setTarefas(data);
+      } else {
+        console.error("Erro ao buscar tarefas:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar tarefas:", error);
+    }
+  };
+
+  const handleExcluirTarefa = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/task/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        console.log("Tarefa deletada");
+        setTarefas(tarefas.filter((tarefa) => tarefa.id !== id));
+      } else {
+        console.error("Erro ao deletar a tarefa:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Erro ao deletar a tarefa:", error);
+    }
+  };
+
   return (
-    <div className="Todo">
-      <p className="completed">Tarefa 1</p>
-      <div>
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} />
-      </div>
+    <div className="tarefa-list-card">
+      <h2>Suas Tarefas</h2>
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa.id} className="tarefa-item">
+            <span>{tarefa.description}</span>
+            <button onClick={() => handleExcluirTarefa(tarefa.id)}>
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
+++ src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
@@ -2,9 +2,26 @@ package com.labdessoft.roteiro01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Roteiro01Application {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+ 
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*"); 
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Roteiro01Application.class, args);
